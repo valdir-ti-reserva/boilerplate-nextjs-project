af52880e15cf6ecdd2619c9ab1fb28ba9ebd0925
Valdir Silva <v.souza.da.silva@avanade.com>
2021-11-15 22:51:46 -0300

    feat: create the first component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 const Main = () => (
-    <main>
-        <h1>React Avançado</h1>
-    </main>
+    <S.Wrapper>
+        <S.Logo
+            src="/img/logo.svg"
+            alt="Atom image and react advanced write besides"
+        />
+        <S.Title>React Avançado</S.Title>
+        <S.Description>
+            TypeScript, ReactJS, NextJS and Styled components
+        </S.Description>
+        <S.Illustration
+            src="/img/hero-illustration.svg"
+            alt="A developer working"
+        />
+    </S.Wrapper>
 )
 
 export default Main
